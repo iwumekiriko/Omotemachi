f42818af531bb7993e57567e0d523eb3eddf117a
Сергей Трофимов <sm.trofimov2004@gmail.com>
2025-07-02 13:33:20 +0300

    new exceptions

---
+++ Omotemachi/Exceptions/Wacky/CCG/GiveCommandTimeoutException.cs
@@ -0,0 +1,11 @@
+﻿using NuGet.Packaging.Signing;
+
+namespace Omotemachi.Exceptions.Wacky.CCG;
+
+public class GiveCommandTimeoutException(
+    TimeSpan timeLeft
+) : Exception, ICustomException
+{
+    public string Code { get; set; } = "00f87";
+    public TimeSpan TimeLeft { get; set; } = timeLeft;
+}
---
+++ Omotemachi/Exceptions/Wacky/CCG/NotEnoughCardsException.cs
@@ -0,0 +1,15 @@
+﻿namespace Omotemachi.Exceptions.Wacky.CCG;
+
+public class NotEnoughCardsException(
+    string cardName,
+    string cardSeriesName,
+    int cardsAmount,
+    int cardsNeeded
+) : Exception, ICustomException
+{
+    public string Code { get; set; } = "00f15";
+    public string Name { get; set; } = cardName;
+    public string SeriesName { get; set; } = cardSeriesName;
+    public int Amount { get; set; } = cardsAmount;
+    public int Needed { get; set; } = cardsNeeded;
+}
