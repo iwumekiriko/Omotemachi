d8962e33230a708b8b329fd2a4e1a67b9a36fe58
Сергей Трофимов <sm.trofimov2004@gmail.com>
2026-05-06 19:58:21 +0300

    add logger to builder

---
+++ Omotemachi/Extensions/LoggingExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Omotemachi.Infrastructure.Logging.Logger;
+
+namespace Omotemachi.Extensions;
+
+public static class LoggingExtensions
+{
+    public static IServiceCollection AddLogging(
+        this IServiceCollection services,
+        WebApplicationBuilder builder
+    )
+    {
+        builder.Logging.ClearProviders();
+        builder.Logging.AddConsole();
+
+        services.AddSingleton<LoggerProvider>();
+
+        builder.Logging.Services.AddSingleton<ILoggerProvider>(sp =>
+            sp.GetRequiredService<LoggerProvider>());
+
+        return services;
+    }
+}
