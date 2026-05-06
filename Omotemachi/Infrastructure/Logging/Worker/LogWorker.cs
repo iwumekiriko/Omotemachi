cadcdb92440ba6df338de803cf9ade8831a8b632
Сергей Трофимов <sm.trofimov2004@gmail.com>
2026-05-07 01:04:32 +0300

    add console and db filters to log worker

--- Omotemachi/Infrastructure/Logging/Worker/LogWorker.cs
+++ Omotemachi/Infrastructure/Logging/Worker/LogWorker.cs
@@ -4,6 +4,8 @@ using Omotemachi.Infrastructure.Logging.Queue;
 using Omotemachi.Infrastructure.Persistance.AppContext;
 using Omotemachi.Models.V1.Domain.Jester.Config;
 using Omotemachi.Models.V1.Domain.Logs;
+using Omotemachi.Tools;
+using LogLevel = Omotemachi.Models.V1.Domain.Logs.LogLevel;
 
 namespace Omotemachi.Infrastructure.Logging.Worker;
 
@@ -19,15 +21,20 @@ public class LogWorker(
     {
         await foreach (var log in _queue.ReadAllAsync())
         {
-            using var scope = _services.CreateScope();
-
-            var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-            var dispatcher = scope.ServiceProvider.GetRequiredService<IWebhookDispatcher>();
-
-            await SaveLogAsync(context, log);
-
-            if (!(log.GuildId == 0))
+            if (IsConsoleLoggable(log))
             {
+                WriteConsoleLog(log);
+            }
+
+            using var scope = _services.CreateScope();
+            var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+            if (IsDbLoggable(log))
+                await SaveLogAsync(context, log);
+
+            if (log.Source == LogSource.External && log.GuildId != 0)
+            {
+                var dispatcher = scope.ServiceProvider.GetRequiredService<IWebhookDispatcher>();
                 var config = await GetLogsConfigByGuild(context, log.GuildId);
                 await dispatcher.DispatchAsync(log, config);
             }
@@ -52,5 +59,58 @@ public class LogWorker(
         }
 
         return config;
+    }
+    private static bool IsDbLoggable(LogEntry log)
+    {
+        return log.Source switch
+        {
+            LogSource.Framework => log.Level >= LogLevel.Warning,
+            LogSource.Application => log.Level >= LogLevel.Information,
+            LogSource.External => log.Level >= LogLevel.Information,
+            _ => false
+        };
+    }
+    private static bool IsConsoleLoggable(LogEntry log)
+    {
+        return log.Source switch
+        {
+            LogSource.Framework => log.Level >= LogLevel.Information,
+            LogSource.Application => log.Level >= LogLevel.Debug,
+            LogSource.External => log.Level >= LogLevel.Information,
+            _ => false
+        };
+    }
+    private static ConsoleColor GetLevelColor(LogLevel level)
+    {
+        return level switch
+        {
+            LogLevel.Debug => ConsoleColor.Cyan,
+            LogLevel.Information => ConsoleColor.Green,
+            LogLevel.Warning => ConsoleColor.Yellow,
+            LogLevel.Error => ConsoleColor.DarkRed,
+            LogLevel.Critical => ConsoleColor.Red,
+            _ => ConsoleColor.White
+        };
+    }
+    private static void WriteConsoleLog(LogEntry log)
+    {
+        var timestamp = TimeConverter.GetCurrentTime();
+        var originalColor = Console.ForegroundColor;
+
+        if (log.Source == LogSource.External)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write("[EXTERNAL] ");
+            Console.ForegroundColor = originalColor;
+            Console.Write("| ");
+        }
+
+        Console.Write($"[{timestamp}] ");
+
+        Console.ForegroundColor = GetLevelColor(log.Level);
+        Console.Write(log.Level.ToString().ToUpper());
+        Console.ForegroundColor = originalColor;
+
+        Console.WriteLine($": {log.Message}");
     }
 }
