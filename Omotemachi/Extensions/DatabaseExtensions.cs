8eaa7195ec7a2f0d42396842af909b29dccb7eec
Сергей Трофимов <sm.trofimov2004@gmail.com>
2026-05-06 19:57:19 +0300

    add separate service collection extenstion classes

---
+++ Omotemachi/Extensions/DatabaseExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Omotemachi.Infrastructure.Persistance.AppContext;
+
+namespace Omotemachi.Extensions;
+
+public static class DatabaseExtensions
+{
+    public static IServiceCollection AddDatabase(this IServiceCollection services, string connectionString)
+    {
+        services.AddDbContext<AppDbContext>(options =>
+        
+            options.UseNpgsql(connectionString, npgsql =>
+            {
+                npgsql.MigrationsAssembly(typeof(AppDbContext).Assembly.FullName);
+            }));
+
+        return services;
+    }
+}
--- Omotemachi/Extensions/ServiceCollectionExtensions.cs
+++ Omotemachi/Extensions/ServiceCollectionExtensions.cs
@@ -1,5 +1,10 @@
-﻿using Omotemachi.Models.V1.Domain.Jester.Config;
+﻿using Omotemachi.Infrastructure.Dispatchers.Webhooks;
+using Omotemachi.Infrastructure.Logging.Queue;
+using Omotemachi.Models.V1.Domain.Jester.Config;
+using Omotemachi.Services;
 using Omotemachi.Services.Jester;
+using Omotemachi.Services.Logs;
+using Omotemachi.Services.Wacky;
 using System.Reflection;
 
 namespace Omotemachi.Extensions;
@@ -19,6 +24,35 @@ public static class ServiceCollectionExtensions
 
             services.AddScoped(serviceType, implementationType);
         }
+
+        return services;
+    }
+        
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        services.AddSingleton<Dictionary<string, int>>();
+
+        services.AddScoped<IMembersService, MembersService>();
+        services.AddScoped<IUserSettingsService, UserSettingsService>();
+        services.AddScoped<IEconomyService, EconomyService>();
+        services.AddScoped<IInventoryService, InventoryService>();
+        services.AddScoped<ILootboxesService, LootboxesService>();
+        services.AddScoped<IShopService, ShopService>();
+        services.AddScoped<IQuestsService, QuestsService>();
+        services.AddScoped<ITicketsService, TicketsService>();
+        services.AddScoped<IDuetsService, DuetsService>();
+        services.AddScoped<IStatisticsService, StatisticsService>();
+        services.AddScoped<IInteractionsService, InteractionsService>();
+        services.AddScoped<ITopService, TopService>();
+        services.AddScoped<IDNDService, DNDService>();
+        services.AddScoped<ICCGService, CCGService>();
+        services.AddScoped<IAppaService, AppaService>();
+        services.AddScoped<IPacksService, PacksService>();
+        AddConfigServices(services);
+
+        services.AddScoped<IWebhookDispatcher, WebhookDispatcher>();
+        services.AddScoped<ILogsService, LogsService>();
+        services.AddSingleton<ILogQueue, LogQueue>();
 
         return services;
     }
