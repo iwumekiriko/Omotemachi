e2de27aa0adad3fd4ce036c1ff7d603e371bce0a
Сергей Трофимов <sm.trofimov2004@gmail.com>
2026-05-06 14:21:39 +0300

    add WebhookDispatcher

---
+++ Omotemachi/Infrastructure/Dispatchers/Webhooks/WebhookDispatcher.cs
@@ -0,0 +1,38 @@
+﻿using Omotemachi.Models.V1.Domain.Jester.Config;
+using Omotemachi.Models.V1.Domain.Logs;
+
+namespace Omotemachi.Infrastructure.Dispatchers.Webhooks;
+
+public interface IWebhookDispatcher
+{
+    Task DispatchAsync(LogEntry log, LogsConfig config);
+}
+
+public class WebhookDispatcher(
+    IHttpClientFactory httpFactory
+) : IWebhookDispatcher
+{
+    private readonly IHttpClientFactory _httpFactory = httpFactory;
+
+    public async Task DispatchAsync(LogEntry log, LogsConfig config)
+    {
+        if (log.GuildId == 0) return;
+
+        var url = config.GetWebhookUrl(log.Category ?? "else");
+        if (string.IsNullOrEmpty(url)) return;
+
+        await SendWebhook(url, log);
+    }
+
+    private async Task SendWebhook(string url, LogEntry log)
+    {
+        var client = _httpFactory.CreateClient();
+
+        var payload = new
+        {
+            content = $"[{log.Level}] {log.Message}"
+        };
+
+        await client.PostAsJsonAsync(url, payload);
+    }
+}
