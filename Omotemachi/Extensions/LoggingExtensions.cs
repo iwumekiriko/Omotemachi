64a93def3ddde1cafb8b0905d6942dad6adf3ee7
Сергей Трофимов <sm.trofimov2004@gmail.com>
2026-05-07 01:01:15 +0300

    refactor remove filters from logging extenstions

--- Omotemachi/Extensions/LoggingExtensions.cs
+++ Omotemachi/Extensions/LoggingExtensions.cs
@@ -10,12 +10,6 @@ public static class LoggingExtensions
     )
     {
         builder.Logging.ClearProviders();
-
-        builder.Logging.AddFilter("Microsoft", LogLevel.Warning);
-        builder.Logging.AddFilter("Microsoft.Hosting.Lifetime", LogLevel.Warning);
-        builder.Logging.AddFilter<LoggerProvider>(level =>
-            level >= LogLevel.Information);
-
         services.AddSingleton<LoggerProvider>();
 
         builder.Logging.Services.AddSingleton<ILoggerProvider>(sp =>
