c77b94654e6cf18a9d5e49c2d5f6741dc29f7596
Сергей Трофимов <sm.trofimov2004@gmail.com>
2026-05-06 14:21:05 +0300

    add LogQueue

---
+++ Omotemachi/Infrastructure/Logging/Queue/LogQueue.cs
@@ -0,0 +1,25 @@
+﻿using Omotemachi.Models.V1.Domain.Logs;
+using System.Threading.Channels;
+
+namespace Omotemachi.Infrastructure.Logging.Queue;
+
+public interface ILogQueue
+{
+    void Enqueue(LogEntry log);
+    IAsyncEnumerable<LogEntry> ReadAllAsync();
+}
+
+public class LogQueue : ILogQueue
+{
+    private readonly Channel<LogEntry> _channel = Channel.CreateUnbounded<LogEntry>();
+
+    public void Enqueue(LogEntry log)
+    {
+        _channel.Writer.TryWrite(log);
+    }
+
+    public IAsyncEnumerable<LogEntry> ReadAllAsync()
+    {
+        return _channel.Reader.ReadAllAsync();
+    }
+}
