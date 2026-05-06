723f8a567292908fe8dcb255318c46769b40a50c
Сергей Трофимов <sm.trofimov2004@gmail.com>
2026-05-06 14:21:58 +0300

    add Logger and LoggerProvider

---
+++ Omotemachi/Infrastructure/Logging/Logger/Logger.cs
@@ -0,0 +1,54 @@
+﻿using Omotemachi.Infrastructure.Logging.Queue;
+using Omotemachi.Models.V1.Domain.Logs;
+
+namespace Omotemachi.Infrastructure.Logging.Logger;
+
+using MsLogLevel = Microsoft.Extensions.Logging.LogLevel;
+using AppLogLevel = Models.V1.Domain.Logs.LogLevel;
+
+public class Logger(string categoryName, ILogQueue queue) : ILogger
+{
+    private readonly ILogQueue _queue = queue;
+    private readonly string _categoryName = categoryName;
+
+    public IDisposable? BeginScope<TState>(TState state) => null;
+
+    public bool IsEnabled(MsLogLevel logLevel) => true;
+
+    public void Log<TState>(
+        MsLogLevel logLevel,
+        EventId eventId,
+        TState state,
+        Exception? exception,
+        Func<TState, Exception?, string> formatter
+    )
+    {
+        var message = formatter(state, exception);
+
+        var log = new LogEntry 
+        {
+            Id = Guid.NewGuid(),
+            Message = message,
+            Level = MapLevel(logLevel),
+            Source = LogSource.API,
+            Category = _categoryName,
+            GuildId = 0
+        };
+
+        _queue.Enqueue(log);
+    }
+
+    private static AppLogLevel MapLevel(MsLogLevel level)
+    {
+        return level switch
+        {
+            MsLogLevel.Trace => AppLogLevel.Debug,
+            MsLogLevel.Debug => AppLogLevel.Debug,
+            MsLogLevel.Information => AppLogLevel.Info,
+            MsLogLevel.Warning => AppLogLevel.Warning,
+            MsLogLevel.Error => AppLogLevel.Error,
+            MsLogLevel.Critical => AppLogLevel.Error,
+            _ => AppLogLevel.Info
+        };
+    }
+}
---
+++ Omotemachi/Infrastructure/Logging/Logger/LoggerProvider.cs
@@ -0,0 +1,17 @@
+﻿using Omotemachi.Infrastructure.Logging.Queue;
+
+namespace Omotemachi.Infrastructure.Logging.Logger;
+
+public class LoggerProvider(ILogQueue queue) : ILoggerProvider
+{
+    private readonly ILogQueue _queue = queue;
+
+    public ILogger CreateLogger(string categoryName)
+    {
+        return new Logger(categoryName, _queue);
+    }
+    public void Dispose() 
+    { 
+        GC.SuppressFinalize(this);
+    }
+}
